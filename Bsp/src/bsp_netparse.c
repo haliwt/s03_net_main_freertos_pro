522244a73a99fcad071a99b00c4abcc5b06bc6b6
haliwt <jonewang@126.com>
2025-01-06 16:21:10 +0800

    update

--- Bsp/src/bsp_netparse.c
+++ Bsp/src/bsp_netparse.c
@@ -35,3 +35,42 @@ void net_parse_beijint_time(void)
 
 }
 #endif 
+
+
+#if 0
+
+
+#define BUFFER_SIZE 256
+
+uint8_t rxBuffer[BUFFER_SIZE];
+uint8_t dataBuffer[BUFFER_SIZE];
+uint8_t rxData;
+int bufferIndex = 0;
+int paramsFound = 0;
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+    if (huart->Instance == USART1) {
+        // 将接收的数据存入缓冲区
+        rxBuffer[bufferIndex++] = rxData;
+        
+        // 检查缓冲区是否包含 "params"
+        if (!paramsFound && bufferIndex >= 6 && strncmp((char *)&rxBuffer[bufferIndex - 6], "params", 6) == 0) {
+            paramsFound = 1;
+            bufferIndex = 0;
+        }
+
+        // 如果找到 "params"，将后续数据存入 dataBuffer
+        if (paramsFound) {
+            dataBuffer[bufferIndex++] = rxData;
+        }
+
+        // 再次启用接收中断
+        HAL_UART_Receive_IT(&huart1, &rxData, 1);
+    }
+}
+
+
+
+
+
+#endif 
